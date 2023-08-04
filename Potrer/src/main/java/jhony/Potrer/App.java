291b8576a0212d46163d02c4c9c1cec7975ee6f7
jhanavarshan <141221629+jhanavarshan@users.noreply.github.com>
2023-08-04 14:10:37 +0530

    initial commit

---
+++ Potrer/src/main/java/jhony/Potrer/App.java
@@ -0,0 +1,13 @@
+package jhony.Potrer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
